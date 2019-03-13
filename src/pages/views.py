c5ba46f23c5d68934459d4a080a5cd44d0d2a4a8
alan <alanbignon2@gmail.com>
2019-03-13 13:40:12 +0100

    modif pages/views

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
-def homepage_view():
-    return "<h1>Hello World</h1>"
+def home_view(*args, **kwargs):
+    return HttpResponse("<h1>Hello World</h1>")
