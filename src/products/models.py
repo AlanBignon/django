7c4912ad1ed094422965a365219d3a16fca8a0f0
alan <alanbignon2@gmail.com>
2019-03-13 13:06:17 +0100

    modif models

--- src/products/models.py
+++ src/products/models.py
@@ -5,5 +5,5 @@ class Product(models.Model):
     title = models.CharField(max_length=120)  # max_lentgth is required
     description = models.TextField(blank=True, null=True)
     price = models.DecimalField(max_digits=65, decimal_places=2)
-    summary = models.TextField()
+    summary = models.TextField(blank=False, null=False)
     featured = models.BooleanField()  # null=True, default=True
