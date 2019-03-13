fb86786ac7d9788639d00d286416a987add68dad
alan <alanbignon2@gmail.com>
2019-03-13 12:35:56 +0100

    first commit

---
+++ src/products/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Product(models.Model):
+    title = models.CharField(max_length=120)  # max_lentgth is required
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=65, decimal_places=2)
+    summary = models.TextField()
+    featured = models.BooleanField()  # null=True, default=True
