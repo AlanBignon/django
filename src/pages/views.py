408f8385852931af17c7000a6d482b5a09568829
alan <alanbignon2@gmail.com>
2019-03-14 18:28:34 +0100

    ajout des templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -6,18 +6,22 @@ from django.http import HttpResponse
 def home_view(request, *args, **kwargs):
     print(args, kwargs)
     print(request.user)
-    return HttpResponse("<h1>Hello World</h1>")
+    # return HttpResponse("<h1>Hello World</h1>")
+    return render(request, "home.html")
 
 
 def contact_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Page des contact</h1>")
+    # return HttpResponse("<h1>Page des contact</h1>")
+    return render(request, "contact.html")
 
 
 def about_view(request, *args, **kwargs):
-    return HttpResponse("<h1>About</h1>")
+    # return HttpResponse("<h1>About</h1>")
+    return render(request, "about.html")
 
 
 def social_view(request, *args, **kwargs):
-    return HttpResponse("<h1>Social views</h1>")
+    # return HttpResponse("<h1>Social views</h1>")
+    return render(request, "social.html")
 
 
