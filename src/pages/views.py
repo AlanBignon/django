0bbbf7bfb34eb749e82f7e589727eb4a619fc963
alan <alanbignon2@gmail.com>
2019-03-15 09:51:06 +0100

    ajout de la base des templates

--- src/pages/views.py
+++ src/pages/views.py
@@ -1,27 +1,26 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    print(args, kwargs)
-    print(request.user)
-    # return HttpResponse("<h1>Hello World</h1>")
     return render(request, "home.html")
 
 
 def contact_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>Page des contact</h1>")
     return render(request, "contact.html")
 
 
 def about_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>About</h1>")
-    return render(request, "about.html")
+    my_context = {
+        "my_text": "this is about me",
+        "this_is_true": True,
+        "my_number": 123,
+        "my_list": [123, 453, "Abc"]
+    }
+    return render(request, "about.html", my_context)
 
 
 def social_view(request, *args, **kwargs):
-    # return HttpResponse("<h1>Social views</h1>")
     return render(request, "social.html")
 
 
